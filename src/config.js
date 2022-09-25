95d61442de0acda8bc38fe42c11f6eca4ab42995
Kapil Soni <kapilsoni54768161@gmail.com>
2022-09-25 10:54:16 +0530

    .

--- src/config.js
+++ src/config.js
@@ -1,6 +1,4 @@
-// export const   nftaddress = '0x5da43a978A76A7539656C55d110466A87CAdfEF6'
-// export const  marketplaceAddress = '0xC8260Ba967dC01F2754Ca420Cf5a7335F90a8E82'
+export const   nftaddress = ''
+export const  marketplaceAddress = ''
 
-export const   nftaddress = '0x575c77Be10B525Af21d2394BDc3e24E4f6A960Dd'
-export const  marketplaceAddress = '0x5739D8E792B695e6BD6D7C6928450906715113B4'
 // export default {nftaddress , marketplaceAddress}
